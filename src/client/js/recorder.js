d16c3868aba4a62c188233dbcb7ed78524b7e5bd
Seungoh <styu12@naver.com>
2022-02-02 17:46:15 +0900

    Video Transcoding webm to mp4

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,5 +1,6 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
 let stream;
 let recorder;
@@ -40,12 +41,46 @@ const handleStop = () => {
   startBtn.addEventListener("click", handleDownload);
 };
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({
+    log: true,
+    corePath: "/ffmpeg/ffmpeg-core.js",
+  });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoUrl));
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
+
   const a = document.createElement("a");
-  a.href = videoUrl;
-  a.download = "MyRecording.webm";
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "thumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 startBtn.addEventListener("click", handleStart);
--- src/server.js
+++ src/server.js
@@ -30,6 +30,12 @@ app.use(localsMiddleware);
 
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
+app.use("/ffmpeg", express.static("node_modules/@ffmpeg/core/dist"));
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
