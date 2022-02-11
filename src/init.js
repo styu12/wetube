198d4e50bd2f33b2c76ac57d4661b4fbf31a7442
Seungoh <styu12@naver.com>
2022-02-12 00:17:49 +0900

    heroku PORT

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User.js";
 import "./models/Comment.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const listenHandler = () =>
   console.log(`✅Server listening on port http://localhost:${PORT} 🎇`);
 
